e3709b5a31546cc6a522b39488604e3c3804518d
samugd17 <samugd17@gmail.com>
2023-10-30 23:31:56 +0000

    se sube solución DWEC03

--- tareas/DWEC03/script.js
+++ tareas/DWEC03/script.js
@@ -1,20 +1,20 @@
 function openNewWindow() {
     var newWindow = window.open('', '', 'width=800,height=600,resizable=no');
   
-    // Write content in the new window
+    // Writing content in the new window
     newWindow.document.write('<h1>Ejemplo de Ventana Nueva</h1>');
-    newWindow.document.write('<b>Full URL:</b> ' + newWindow.location.href + '<br>');
+    newWindow.document.write('<b>Full URL:</b> ' + newWindow.location + '<br>');
     newWindow.document.write('<b>Protocol used:</b> ' + newWindow.location.protocol + '<br>');
     newWindow.document.write('<b>Browser Code Name:</b> ' + navigator.userAgent + '<br>');
   
-    // Check if Java is available in the new window
+    // Checking if Java is available in the new window (deprecated)
     if (newWindow.navigator.javaEnabled()) {
       newWindow.document.write('Java IS available in this window.<br>');
     } else {
       newWindow.document.write('Java IS NOT available in this window.<br>');
     }
   
-    // Open an iframe with the content of www.google.com at 800x600
+    // Opening an iframe with the content of www.bing.com at 800x600
     newWindow.document.write('<iframe src="https://www.bing.com" width="800" height="600"></iframe>');
   }
   
--- tareas/DWEC03/script2.js
+++ tareas/DWEC03/script2.js
@@ -1,10 +1,12 @@
 function printData(){
     document.write("<h1>TAREA DWEC03</h1><hr/>")
     let fullName = prompt("Nombre y apellidos: ");
-    let day = prompt("Introduzca su dia de nacimiento: ");
-    let month = prompt("Introduzca su mes de nacimiento: ");
-    let year = prompt("Introduzca su año de nacimiento: ");
+    let birthDate = new Date(prompt("Introduzca su fecha de nacimiento (YYYY-MM-DD): "));
+    
     let actualDate = new Date();
+    let years = actualDate.getFullYear() - birthDate.getFullYear();
+    let actualMonth = actualDate.getMonth();
+    let birthMonth = birthDate.getMonth();
 
     document.write('<p>Buenos días ' + fullName + '<br><br>')
     document.write('<p>Tu nombre tiene ' + fullName.length + ' caracteres, incluidos espacios' + '<br><br>')
@@ -12,8 +14,11 @@ function printData(){
     document.write('La última letra A de tu nombre está en la posición: ' + fullName.toLowerCase().lastIndexOf('a') + '<br><br>')
     document.write('Tu nombre menos las 3 primeras letras es: ' + fullName.slice(3) +'<br><br>')
     document.write('Tu nombre todo en mayúsculas es: ' + fullName.toUpperCase() + '<br><br>')
-    document.write('Tu edad es: ' + actualDate.getFullYear() - year + ' años. ' + '<br><br>')
-    document.write('Naciste un feliz ' + day + '/' + month + ' del año ' + year + '.' + '<br><br>')
+    if (actualMonth < birthMonth || (actualMonth === birthMonth && actualDate.getDate() < birthDate.getDate())) {
+        years--;
+      }
+    document.write('Tu edad es: ' + years + ' años. ' + '<br><br>')
+    document.write('Naciste un feliz ' + birthDate.getDate() + '/' + birthMonth + ' del año ' + birthDate.getFullYear() + '.' + '<br><br>')
     document.write('El coseno de 180 es: ' + Math.cos(180) + '<br><br>')
     document.write('El número mayor de (34,67,23,75,35,19) es: ' + Math.max(34,67,23,75,35,19) + '<br><br>')
     document.write('Ejemplo de número al azar: ' + Math.random() + '<br><br>')      
