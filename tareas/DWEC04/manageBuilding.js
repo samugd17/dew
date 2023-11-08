9a321525c2fc178d121c2c5be76b10bc6a369d10
samugd17 <samugd17@gmail.com>
2023-11-08 12:24:16 +0000

    DWEC04

---
+++ tareas/DWEC04/manageBuilding.js
@@ -0,0 +1,9 @@
+class Building {
+    constructor(street, streetNumber, zipCode, floors) {
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.zipCode = zipCode;
+        this.floors = floors;
+    }
+ 
+}
