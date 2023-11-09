a2befa56bb5de71dc643b2ffb86cdc62c4029719
samugd17 <samugd17@gmail.com>
2023-11-09 01:22:01 +0000

    DWEC04 added

--- tareas/DWEC04/manageBuilding.js
+++ tareas/DWEC04/manageBuilding.js
@@ -1,87 +1,87 @@
-class Building {
-    constructor(street, streetNumber, zipCode) {
-        this.street = street;
-        this.streetNumber = streetNumber;
-        this.zipCode = zipCode;
-        this.floors = [];
-        console.log(`Construido nuevo edificio en: ${street}, nº: ${streetNumber}, CP: ${zipCode}.`);
+class Edificio {
+    constructor(calle, numero, codigoPostal) {
+        this.calle = calle;
+        this.numero = numero;
+        this.codigoPostal = codigoPostal;
+        this.plantas = [];
+        console.log(`Construido nuevo edificio en calle: ${calle}, nº: ${numero}, CP: ${codigoPostal}.`);
     }
 
-    addFloorsAndDoors(numFloors, doorsPerFloor) {
-        for (let i = 0; i < numFloors; i++) {
-            let floor = { doors: [] };
+    agregarPlantasYPuertas(numPlantas, puertasPorPlanta) {
+        for (let i = 0; i < numPlantas; i++) {
+            let planta = { puertas: [] };
 
-            for (let j = 0; j < doorsPerFloor; j++) {
-                floor.doors.push({ owner: null });
+            for (let j = 0; j < puertasPorPlanta; j++) {
+                planta.puertas.push({ propietario: null });
             }
 
-            this.floors.push(floor);
+            this.plantas.push(planta);
         }
-        console.log(`Added ${numFloors} floors with ${doorsPerFloor} doors each.`);
+        console.log(`Agregadas ${numPlantas} plantas con ${puertasPorPlanta} puertas cada una.`);
     }
 
-    modifyNumber(number) {
-        this.streetNumber = number;
-        console.log(`Building number updated to ${number}.`);
+    modificarNumero(numero) {
+        this.numero = numero;
+        console.log(`Número del edificio actualizado a ${numero}.`);
     }
 
-    modifyStreet(street) {
-        this.street = street;
-        console.log(`Street name updated to ${street}.`);
+    modificarCalle(calle) {
+        this.calle = calle;
+        console.log(`Nombre de la calle actualizado a ${calle}.`);
     }
 
-    modifyZipCode(code) {
-        this.zipCode = code;
-        console.log(`Building ZIP code updated to ${code}.`);
+    modificarCodigoPostal(codigo) {
+        this.codigoPostal = codigo;
+        console.log(`Código postal del edificio actualizado a ${codigo}.`);
     }
 
-    printStreet() {
-        console.log(`The street of the building is: ${this.street}.`);
+    imprimeCalle() {
+        console.log(`La calle del edificio es: ${this.calle}.`);
     }
 
-    printNumber() {
-        console.log(`The number of the building is: ${this.streetNumber}.`);
+    imprimeNumero() {
+        console.log(`El número del edificio es: ${this.numero}.`);
     }
 
-    printZipCode() {
-        console.log(`The ZIP code of the building is: ${this.zipCode}.`);
+    imprimeCodigoPostal() {
+        console.log(`El código postal del edificio es: ${this.codigoPostal}.`);
     }
 
-    addOwner(name, floor, door) {
-        if (this.floors[floor - 1] && this.floors[floor - 1].doors[door - 1]) {
-            this.floors[floor - 1].doors[door - 1].owner = name;
-            console.log(`${name} is now the owner of door ${door} on floor ${floor}.`);
+    agregarPropietario(nombre, planta, puerta) {
+        if (this.plantas[planta - 1] && this.plantas[planta - 1].puertas[puerta - 1]) {
+            this.plantas[planta - 1].puertas[puerta - 1].propietario = nombre;
+            console.log(`${nombre} es ahora el propietario de la puerta ${puerta} de la planta ${planta}.`);
         } else {
-            console.log("Error: Floor or door does not exist.");
+            console.log("Error: Planta o puerta no existente.");
         }
     }
 
-    printFloors() {
-        console.log(`List of owners of the building on ${this.street} number ${this.streetNumber}`);
+    imprimePlantas() {
+        console.log(`Listado de propietarios del edificio ${this.calle} número ${this.numero}`);
 
-        for (let i = 0; i < this.floors.length; i++) {
-            for (let j = 0; j < this.floors[i].doors.length; j++) {
-                const owner = this.floors[i].doors[j].owner || "";
-                console.log(`Owner of door ${j + 1} on floor ${i + 1}: ${owner}.`);
+        for (let i = 0; i < this.plantas.length; i++) {
+            for (let j = 0; j < this.plantas[i].puertas.length; j++) {
+                const propietario = this.plantas[i].puertas[j].propietario || "";
+                console.log(`Propietario del piso ${j + 1} de la planta ${i + 1}: ${propietario}.`);
             }
         }
     }
 }
 
-// Example usage
-const buildingA = new Building("Garcia Prieto", 58, "15706");
-const buildingB = new Building("Camino Caneiro", 29, "32004");
-const buildingC = new Building("San Clemente", "s/n", "15705");
+// Ejemplos
+const edificioA = new Edificio("Garcia Prieto", 58, "15706");
+const edificioB = new Edificio("Camino Caneiro", 29, "32004");
+const edificioC = new Edificio("San Clemente", "s/n", "15705");
 
-buildingA.addFloorsAndDoors(2, 3);
-buildingA.addOwner("Jose Antonio Lopez", 1, 1);
-buildingA.addOwner("Luisa Martinez", 1, 2);
-buildingA.addOwner("Marta Castellón", 1, 3);
-buildingA.addOwner("Antonio Pereira", 2, 2);
+edificioA.agregarPlantasYPuertas(2, 3);
+edificioA.agregarPropietario("Jose Antonio Lopez", 1, 1);
+edificioA.agregarPropietario("Luisa Martinez", 1, 2);
+edificioA.agregarPropietario("Marta Castellón", 1, 3);
+edificioA.agregarPropietario("Antonio Pereira", 2, 2);
 
-buildingA.printFloors();
+edificioA.imprimePlantas();
 
-buildingA.addFloorsAndDoors(1, 0);
-buildingA.addOwner("Pedro Meijide", 3, 2);
+edificioA.agregarPlantasYPuertas(1, 0);
+edificioA.agregarPropietario("Pedro Meijide", 3, 2);
 
-buildingA.printFloors();
+edificioA.imprimePlantas();
