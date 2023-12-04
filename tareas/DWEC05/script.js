a16947e0b61404758fb515c8680f6f0853ab0113
samugd17 <samugd17@gmail.com>
2023-12-04 20:22:52 +0000

    Añadido validación por edad

--- tareas/DWEC05/script.js
+++ tareas/DWEC05/script.js
@@ -2,13 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // Función para convertir a mayúsculas los campos de Nombre y Apellidos
-    function convertirAMayusculas(event) {
-      event.target.value = event.target.value.toUpperCase();
+    function convertToUppercase(event) {
+    	event.target.value = event.target.value.toUpperCase();
     }
   
     // Función para validar los campos del formulario
     function validarFormulario(event) {
-      event.preventDefault(); // Evitar el envío automático del formulario
+    	event.preventDefault(); // Evitar el envío automático del formulario
   
       // Obtiene referencias a los elementos del formulario
       const nombre = document.getElementById('nombre');
@@ -23,48 +23,64 @@ document.addEventListener('DOMContentLoaded', function () {
       const erroresContainer = document.getElementById('errores');
       const intentosContainer = document.getElementById('intentos');
   
-      // Validación de Nombre y Apellidos
-      if (nombre.value.trim() === '' || apellidos.value.trim() === '') {
-        erroresContainer.innerHTML = 'Por favor, complete los campos Nombre y Apellidos.';
-        if (nombre.value.trim() === '') {
-          nombre.focus();
-        } else {
-          apellidos.focus();
-        }
-        return;
-      }
+		// Validación de Nombre y Apellidos
+		if (nombre.value.trim() === '' || apellidos.value.trim() === '') {
+			if (nombre.value.trim() === '') {
+				nombre.focus();
+				erroresContainer.innerHTML = 'Por favor, complete el campo: Nombre.';
+
+			} else if (apellidos.value.trim() === '') {
+				apellidos.focus();
+				erroresContainer.innerHTML = 'Por favor, complete el campo: Apellidos.';
+
+			} 
+			return;
+		}
   
-      // Otras validaciones similares para cada campo (Edad, NIF, Email, Provincia, Fecha, Teléfono, Hora)
-  
-      // Incrementar el número de intentos y almacenarlo en una cookie
-      let intentos = 1;
-      if (document.cookie.includes('intentosEnvio')) {
-        intentos = parseInt(getCookie('intentosEnvio')) + 1;
-      }
-      document.cookie = `intentosEnvio=${intentos}`;
-  
-      // Mostrar el número de intentos en el contenedor correspondiente
-      intentosContainer.innerHTML = `Intento de Envíos del formulario: ${intentos}`;
-  
-      // Pedir confirmación de envío
-      const confirmacion = confirm('¿Estás seguro de enviar el formulario?');
-      if (confirmacion) {
-        // Si se confirma, enviar el formulario
-        event.target.submit();
-      } else {
-        // Si se cancela, no enviar el formulario
-        return;
-      }
+		// Validación de Edad
+		if (edad.value.trim() === '') {
+			erroresContainer.innerHTML = 'Por favor, rellene el campo: Edad';
+			edad.focus();
+			return false;
+		}
+
+		if (isNaN(edad.value)) {
+			erroresContainer.innerHTML = 'Por favor, inserte un valor numérico';
+			edad.focus();
+			return false;
+
+		} else if (!(edad.value > 0 && edad.value < 105)) {
+			erroresContainer.innerHTML = 'Usted sólo puede tener 105 años como máximo, deje ya este mundo. Gracias.';
+			edad.focus();
+			return false;
+		}
+		
+	
+		// Incrementar el número de intentos y almacenarlo en una cookie
+		let intentos = 1;
+		if (document.cookie.includes('intentosEnvio')) {
+			intentos = parseInt(getCookie('intentosEnvio')) + 1;
+		}
+		document.cookie = `intentosEnvio=${intentos}`;
+	
+		// Mostrar el número de intentos en el contenedor correspondiente
+		intentosContainer.innerHTML = `Intento de Envíos del formulario: ${intentos}`;
+	
+		// Pedir confirmación de envío
+		const confirmacion = confirm('¿Estás seguro de enviar el formulario?');
+		if (confirmacion) {
+			event.target.submit();
+		} else {
+			return false;
+		}
     }
   
     // Obtener referencia al formulario
     const formulario = document.getElementById('formulario');
   
-    // Agregar event listeners para los campos Nombre y Apellidos
-    document.getElementById('nombre').addEventListener('blur', convertirAMayusculas);
-    document.getElementById('apellidos').addEventListener('blur', convertirAMayusculas);
-  
-    // Agregar event listener para el evento submit del formulario
+    // Agregar event listeners para los campos nombre y apellidos y envío del formulario.
+    nombre.addEventListener('blur', convertToUppercase);
+    apellidos.addEventListener('blur', convertToUppercase);
     formulario.addEventListener('submit', validarFormulario);
   });
   
